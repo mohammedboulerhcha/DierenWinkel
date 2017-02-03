033ce7d45192f5785538b8ceb39d91eb684513be
mobapp10 <mobapp10@10.3.30.125>
2017-02-03 10:41:48 +0100

    Merge origin/master
    Conflicts:
    	DierenWinkel-ejb/nbproject/project.properties
    	DierenWinkel-war/nbproject/project.properties
    	nbproject/project.properties

---
+++ DierenWinkel-war/src/java/be/ehb/beans/SessionBean.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ehb.beans;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateful;
+import javax.inject.Named;
+
+/**
+ *
+ * @author mobapp10
+ */
+@Named
+@Stateful
+public class SessionBean {
+
+    public SessionBean() {
+    }
+   
+    
+   
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ DierenWinkel-war/src/java/be/ehb/beans/ValidatorBean.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ehb.beans;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * met behulp van https://www.mkyong.com/regular-expressions/how-to-validate-email-address-with-regular-expression/
+ * @author mobapp10
+ */
+public class ValidatorBean {
+    Pattern pattern;
+    Matcher matcher;
+    String emailREGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    public ValidatorBean() {
+        pattern = Pattern.compile(emailREGEX);
+    }
+    
+    public boolean validate(final String s) {
+		matcher = pattern.matcher(s);
+		return matcher.matches();
+    }
+}
