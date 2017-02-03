449c51c5bdf35d438d3a3d4d6d3ed8e5e9b8b061
momo <momo@192.168.182.5>
2017-02-03 01:01:21 +0100

    beet je werk op de login pagina en de nieuweaccont pagina

--- DierenWinkel-war/src/java/be/ehb/beans/BeanValidationView.java
+++ DierenWinkel-war/src/java/be/ehb/beans/BeanValidationView.java
@@ -88,32 +88,6 @@ public class BeanValidationView implements Serializable{
         this.E_mail_adres = E_mail_adres;
     }
                
-       public class SelectBooleanView {
- 
-    private boolean value1;   
-    private boolean value2;
- 
-    public boolean isValue1() {
-        return value1;
-    }
- 
-    public void setValue1(boolean value1) {
-        this.value1 = value1;
-    }
- 
-    public boolean isValue2() {
-        return value2;
-    }
- 
-    public void setValue2(boolean value2) {
-        this.value2 = value2;
-    }
-     
-    public void addMessage() {
-        String summary = value2 ? "Checked" : "Unchecked";
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(summary));
-    }
-}      
      
    
 }
