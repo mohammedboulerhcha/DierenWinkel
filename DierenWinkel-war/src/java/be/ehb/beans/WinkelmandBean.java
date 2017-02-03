d0d60bd198b7ba7e30f4483334bb58febc59862c
mobapp10 <mobapp10@10.3.30.125>
2017-02-03 15:22:16 +0100

    Merge origin/master
    Conflicts:
    	DierenWinkel-war/web/NieuweAccount.xhtml
    	DierenWinkel-war/web/index.xhtml

--- DierenWinkel-war/src/java/be/ehb/beans/WinkelmandBean.java
+++ DierenWinkel-war/src/java/be/ehb/beans/WinkelmandBean.java
@@ -6,7 +6,10 @@
 package be.ehb.beans;
 
 import be.ehb.entities.Product;
-import java.util.ArrayList;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.TreeSet;
+
 import javax.ejb.Stateful;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -18,14 +21,18 @@ import javax.inject.Named;
 @Named
 @Stateful
 @SessionScoped
-public class WinkelkarBean {
+public class WinkelmandBean implements Serializable{
     int id;
     String naam;
     float prijs;
-    ArrayList<Product> inhoudKar = new ArrayList<>();
+    HashSet<Product> inhoudmand = new HashSet<>();
     
     
     public void voegToe(Product p) {
-        inhoudKar.add(p);
+        inhoudmand.add(p);
+    }
+    
+    public HashSet<Product> weergeven(Product p) {
+        return inhoudmand;
     }
 }
